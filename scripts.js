75cfc33c0e00f920df21f8b731efd2848da6b0f0
Stephanie Tassone <stephanie.tassone@gmail.com>
2018-04-24 16:18:04 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('#slides').slidesjs({
+    width: 940,
+    height: 800,
+    pagination: {
+      active: false,
+      effect: 'fade'
+    },
+    navigation: {
+      active: true
+    }
+  });
+});
